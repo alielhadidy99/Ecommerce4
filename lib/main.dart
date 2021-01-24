7d55b9f2901ee61b42961c22ddd320c35d5b2994
Ali El-Hadidy <alielhadidy99@gmail.com>
2021-01-24 15:32:37 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+
+import 'package:ecommerce4/provider/category_provider.dart';
+import 'package:ecommerce4/provider/product_provider.dart';
+import 'package:ecommerce4/screens/homepage.dart';
+import 'package:ecommerce4/screens/login.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CategoryProvider>(
+          create: (context) => CategoryProvider(),
+        ),
+        ChangeNotifierProvider<ProductProvider>(
+          create: (context) => ProductProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Color(0xff746bc9),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            } else {
+              return Login();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/search_category.dart
@@ -0,0 +1,96 @@
+
+import 'package:ecommerce4/model/product.dart';
+import 'package:ecommerce4/provider/category_provider.dart';
+import 'package:ecommerce4/widgets/singeproduct.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'detailscreen.dart';
+
+class SearchCategory extends SearchDelegate<void> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    CategoryProvider categoryProvider = Provider.of<CategoryProvider>(context);
+    List<Product> searchCategory = categoryProvider.searchCategoryList(query);
+
+    return GridView.count(
+        childAspectRatio: 0.76,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: searchCategory
+            .map((e) => GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => DetailScreen(
+                          image: e.image,
+                          name: e.name,
+                          price: e.price,
+                        ),
+                      ),
+                    );
+                  },
+                  child: SingleProduct(
+                    image: e.image,
+                    name: e.name,
+                    price: e.price,
+                  ),
+                ))
+            .toList());
+  }
+
+  void getProduct() {}
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    CategoryProvider categoryProvider = Provider.of<CategoryProvider>(context);
+    List<Product> searchCategory = categoryProvider.searchCategoryList(query);
+    return GridView.count(
+        childAspectRatio: 0.87,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: searchCategory
+            .map((e) => GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => DetailScreen(
+                          image: e.image,
+                          name: e.name,
+                          price: e.price,
+                        ),
+                      ),
+                    );
+                  },
+                  child: SingleProduct(
+                    image: e.image,
+                    name: e.name,
+                    price: e.price,
+                  ),
+                ))
+            .toList());
+  }
+}
